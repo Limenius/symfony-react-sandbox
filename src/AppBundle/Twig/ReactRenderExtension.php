8a65a63ff972f64e5c6ac9eb82d46090b3b32fc4
nacho <nacho@limenius.com>
2016-01-27 23:02:07 +0100

    twig function renamed to react_component

--- src/AppBundle/Twig/ReactRenderExtension.php
+++ src/AppBundle/Twig/ReactRenderExtension.php
@@ -8,14 +8,14 @@ class ReactRenderExtension extends \Twig_Extension
     public function getFunctions()
     {
         return array(
-            new \Twig_SimpleFunction('reactServerSide', array($this, 'reactServerSideRender'), array('is_safe' => array('html'))));
+            new \Twig_SimpleFunction('react_component', array($this, 'reactServerSideRender'), array('is_safe' => array('html'))));
     }
 
-    public function reactServerSideRender($options)
+    public function reactServerSideRender($name, $options = array())
     {
         $phpexecjs = new PhpExecJs();
         $phpexecjs->createContextFromFile(dirname(__FILE__).'/../../../server-bundle.js');
-        return json_decode($phpexecjs->evalJs("console.log(ReactOnRails.serverRenderReactComponent({name: '".$options['name']."'}))"))->html;
+        return json_decode($phpexecjs->evalJs("console.log(ReactOnRails.serverRenderReactComponent({name: '".$name."'}))"))->html;
     }
 
     public function getName()
