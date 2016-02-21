6b295dea4a7cd8ccd09622bbb96631d70e1fdd69
nacho <nacho@limenius.com>
2016-02-21 20:08:34 +0100

    Webpack loader for jpegs

--- webpack.config.js
+++ webpack.config.js
@@ -45,7 +45,8 @@ var config = {
             {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
             {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
             {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+            {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
+            {test: /\.jpe?g$/, loader: 'file'}
         ],
     }
 }
