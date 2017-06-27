5ada14f5206a8f25e6bf39bc4155f6a58455e017
Nacho Martín <nacho@limenius.com>
2017-06-27 11:43:47 +0200

    Remove uneeded files

--- client/js/recipes-redux/startup/clientRegistration.js
+++
@@ -1,8 +0,0 @@
-import ReactOnRails from 'react-on-rails'
-import RecipesAppRedux from './RecipesAppClient'
-import configureStore from '../store/RecipesStore'
-
-const recipesStore = configureStore
-
-ReactOnRails.registerStore({ recipesStore })
-ReactOnRails.register({ RecipesAppRedux })
--- client/js/recipes/startup/clientRegistration.js
+++
@@ -1,5 +0,0 @@
-import ReactOnRails from 'react-on-rails'
-import RecipesApp from './RecipesAppClient'
-
-ReactOnRails.register({ RecipesApp })
-
