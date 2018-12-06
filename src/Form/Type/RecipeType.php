93e0dcabe8f3a2bf1603bb363d9b3083832dc6cf
Nacho Martín <nacho@limenius.com>
2018-12-06 16:41:36 +0100

    Not needed import

--- src/Form/Type/RecipeType.php
+++ src/Form/Type/RecipeType.php
@@ -4,7 +4,6 @@ namespace App\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type;
-use Symfony\Component\Validator\Constraints as Assert;
 
 use App\Form\DataTransformer\FileToDataUriTransformer;
 
